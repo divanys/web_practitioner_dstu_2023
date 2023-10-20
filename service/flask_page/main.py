d19de80524fe15b0644ad68cf8580336a7d3fbb3
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-10-20 19:34:12 +0300

    Back #1

--- service/flask_page/main.py
+++ service/flask_page/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 #Объект приложения
@@ -6,7 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
-    return "Main_page"
+    return render_template("index.html")
+
+
+@app.route("/articles")
+def article_page():
+    return render_template("article.html")
+
+@app.route("/create_article")
+def create_article_page():
+    return render_template("create_article.html")
+
+@app.route("/article_show")
+def article_show():
+    return render_template("article_show")
 
 
 
